0eba34aa7789da469d01234c4239971968f22c2c
RiX <jonathan.vogelbusch@gmail.com>
2016-04-14 21:02:08 +0200

    Ignore if Download link equals None
    https://github.com/rix1337/RSScrawler/issues/44

--- RSScrawler.py
+++ RSScrawler.py
@@ -239,6 +239,9 @@ class MovieblogFeed():
                 self.log_info("NEW RELEASE: " + key)
                 download_link = [common.get_first(self._get_download_links(self.dictWithNamesAndLinks[key][0], self._hosters_pattern))]
                 if download_link:
+                    if "none" in download_link.lower
+                        self.log_debug("[%s] no link found for hoster" %key)
+                        continue
                     write_crawljob_file(
                         key,
                         key,
