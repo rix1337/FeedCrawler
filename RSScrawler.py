f6a42119eeba0816da19cc963b8be55eb0ce37b9
RiX <jonathan.vogelbusch@gmail.com>
2016-04-14 21:11:55 +0200

    silly typo

--- RSScrawler.py
+++ RSScrawler.py
@@ -239,7 +239,7 @@ class MovieblogFeed():
                 self.log_info("NEW RELEASE: " + key)
                 download_link = [common.get_first(self._get_download_links(self.dictWithNamesAndLinks[key][0], self._hosters_pattern))]
                 if download_link:
-                    if "none" in str(download_link).lower
+                    if "none" in download_link.lower:
                         self.log_debug("[%s] no link found for hoster" %key)
                         continue
                     write_crawljob_file(
