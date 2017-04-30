b49d3abe07a31d60eb2f94a568f9268c98b09edd
rix <rix@rix.ninja>
2017-04-30 18:11:03 +0000

    v.2.7.0 - Updatehinweis in Webinterface und Console

--- version.py
+++ version.py
@@ -1,6 +1,22 @@
 # -*- coding: utf-8 -*-
 # RSScrawler
 # Projekt von https://github.com/rix1337
+import re
+import urllib2
+from BeautifulSoup import BeautifulSoup
 
 def getVersion():
     return "v.2.7.0"
+
+def updateCheck():
+    # Prüfe, ob lokale Version der aktuellen Entspricht
+    localversion = getVersion()
+    try:
+        # Einzeiler, der die aktuelle Version von GitHub ausliest:
+        onlineversion = str(re.search('return "(.+?)"', BeautifulSoup(urllib2.urlopen('https://raw.githubusercontent.com/rix1337/RSScrawler/master/version.py').read()).findAll(text=re.compile('return "(v\.\d\.\d\.\d)"'))[0]).group(1))
+        if localversion == onlineversion:
+            return (False, localversion)
+        else:
+            return (True, onlineversion)
+    except:
+        return (False, "Error")
