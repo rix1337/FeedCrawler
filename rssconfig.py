9a076e202a2618a0a2e4185f52d8fcdb2dfbd3ee
RiX <rix@rix.ninja>
2017-10-27 08:20:29 +0000

    v.3.1.3 - Die Änderung war wohl nix...

--- rssconfig.py
+++ rssconfig.py
@@ -11,42 +11,42 @@ class RssConfig(object):
     _CONFIG_FILES = [os.path.join(os.path.dirname(sys.argv[0]), 'Einstellungen/RSScrawler.ini')]
     _DEFAULT_CONFIG = {
         'RSScrawler': [
-            ("jdownloader", "str", "Muss unbedingt vergeben werden!"),
-            ("port", "int", "9090"),
-            ("prefix", "str", ""),
-            ("interval", "int", "10"),
-            ("hoster", "str", "Share-Online")
+            ("jdownloader", "str", "", ""),
+            ("port", "int", "", "9090"),
+            ("prefix", "str", "", ""),
+            ("interval", "int", "", ""),
+            ("hoster", "str", "", "")
         ],
         'MB': [
-            ("quality", "str", "720p"),
-            ("ignore", "str", "cam,subbed,xvid,dvdr,untouched,remux,pal,md,ac3md,mic,xxx,hou,h-ou"),
-            ("historical", "bool", "False"),
-            ("regex", "bool", "False"),
-            ("cutoff", "bool", "False"),
-            ("crawl3d", "bool", "False"),
-            ("enforcedl", "bool", "False"),
-            ("crawlseasons", "bool", "True"),
-            ("seasonsquality", "str", "720p"),
-            ("seasonspacks", "bool", "False"),
-            ("seasonssource", "str", "web-dl.*-(tvs|4sj)|webrip.*-(tvs|4sj)|webhd.*-(tvs|4sj)|netflix.*-(tvs|4sj)|amazon.*-(tvs|4sj)|itunes.*-(tvs|4sj)|bluray|bd|bdrip")
+            ("quality", "str", "", ""),
+            ("ignore","str","",""),
+            ("historical","bool","",""),
+            ("regex","bool","", ""),
+            ("cutoff","bool","", ""),
+            ("crawl3d","bool","",""),
+            ("enforcedl", "bool", "", ""),
+            ("crawlseasons", "bool", "", ""),
+            ("seasonsquality", "str", "", ""),
+            ("seasonspacks", "bool", "", ""),
+            ("seasonssource", "str", "", "")
         ],
         'SJ': [
-            ("quality", "str", "720p"),
-            ("rejectlist", "str", "XviD,Subbed,HDTV"),
-            ("regex", "bool", "False")
+            ("quality", "str", "", ""),
+            ("rejectlist", "str", "", ""),
+            ("regex","bool","", "")
         ],
         'YT': [
-            ("youtube", "bool", "False"),
-            ("maxvideos", "int", "10"),
-            ("ignore", "str", "")
+            ("youtube","bool","",""),
+            ("maxvideos","int","",""),
+            ("ignore","str","","")
         ],
         'Notifications': [
-            ("pushbullet", "str", ""),
-            ("pushover", "str", "")
+            ("pushbullet","str","",""),
+            ("pushover","str","","")
         ],
         'Crawljobs': [
-            ("autostart", "str", "True"),
-            ("subdir", "str", "True")
+            ("autostart","str","","True"),
+            ("subdir","str","","True")
         ]
     }
     __config__ = []
@@ -67,7 +67,7 @@ class RssConfig(object):
 
     def _set_default_config(self, section):
         self._config.add_section(section)
-        for (key,key_type,value) in self._DEFAULT_CONFIG[section]:
+        for (key,key_type,comment,value) in self._DEFAULT_CONFIG[section]:
             self._config.set(section,key,value)
         with open(self._CONFIG_FILES[::-1].pop(), 'wb') as configfile:
             self._config.write(configfile)
@@ -76,7 +76,7 @@ class RssConfig(object):
         return [(key, '', '', self._config.get(section,key)) for key in self._config.options(section)]
 
     def _get_from_config(self, scope, key):
-        res = [param[2] for param in scope if param[0] == key]
+        res = [param[3] for param in scope if param[0] == key]
         if [param for param in self._DEFAULT_CONFIG[self._section] if param[0] == key and param[1] == 'bool']:
             return True if len(res) and res[0].strip('\'"').lower() == 'true' else False
         else:
