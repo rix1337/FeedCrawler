98a420dab243b245631115d19ce273218f95714c
Florian Biebel <1ne8ight7even@users.noreply.github.com>
2018-03-09 14:27:03 +0100

    Update notifiers.py
    Added Home Aassistant Method to notifiers

--- notifiers.py
+++ notifiers.py
@@ -25,6 +25,7 @@ def api_request_cutter(l, n):
 
 def notify(added_items):
     notifications = RssConfig('Notifications')
+    homeassistant_settings = notifications.get("homeassistant").split(',')
     pushbullet_token = notifications.get("pushbullet")
     pushover_settings = notifications.get("pushover").split(',')
     items = []
@@ -33,6 +34,11 @@ def notify(added_items):
         items.append(item)
     if len(items) > 0:
         cut_items = list(api_request_cutter(items, 5))
+        if len(notifications.get("homeassistant")) > 0:
+            for cut_item in cut_items:
+                homassistant_url = homeassistant_settings[0]
+                homeassistant_password = homeassistant_settings[1]
+                Homeassistant(cut_item, homassistant_url, homeassistant_password)
         if len(notifications.get("pushbullet")) > 0:
                 Pushbullet(items, pushbullet_token)
         if len(notifications.get('pushover')) > 0:
@@ -41,6 +47,26 @@ def notify(added_items):
                 pushover_token = pushover_settings[1]
                 Pushover(cut_item, pushover_user, pushover_token)
 
+def Homeassistant(items, homassistant_url, homeassistant_password):
+    data = urllib.urlencode({
+        'title': 'RSScrawler:',
+        'body': "\n\n".join(items)
+    })
+    auth = base64.encodestring('%s:' %token).replace('\n', '')
+    try:
+        req = urllib2.Request(homassistant_url, data)
+        req.add_header('X-HA-Access', homeassistant_password % auth)
+        req.add_header('Content-Type', 'application/json')
+        response = urllib2.urlopen(req)
+    except urllib2.HTTPError:
+        log_debug('FEHLER - Konnte Home Assistant API nicht erreichen')
+        return False
+    res = json.load(response)
+    if res['sender_name']:
+        log_debug('Home Assistant Erfolgreich versendet')
+    else:
+        log_debug('FEHLER - Konnte nicht an Home Assistant Senden')    
+                
 def Pushbullet(items, token):
     data = urllib.urlencode({
         'type': 'note',
