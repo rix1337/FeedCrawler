1f6771e99a5c13cc5c8bfb187de8cf5b3183baa8
rix1337 <>
2018-10-05 21:24:40 +0200

    v.5.0.4 - Fix setup.py

--- setup.py
+++ setup.py
@@ -3,26 +3,37 @@
 # Projekt von https://github.com/rix1337
 
 import glob
-import io
 
+import pypandoc
 import setuptools
 
+py = []
+for h in glob.glob("*.py"):
+    py.append(h.replace("\\", "/"))
 web = ["web/index.html"]
 web_css = []
 for i in glob.glob("web/css/*"):
     web_css.append(i.replace("\\", "/"))
 web_img = []
-for h in glob.glob("web/img/*"):
-    web_img.append(h.replace("\\", "/"))
+for j in glob.glob("web/img/*"):
+    web_img.append(j.replace("\\", "/"))
 web_js = []
-for j in glob.glob("web/js/*"):
-    web_js.append(j.replace("\\", "/"))
+for k in glob.glob("web/js/*"):
+    web_js.append(k.replace("\\", "/"))
 
 
 from version import getVersion
 
-with io.open('README.md', encoding='utf-8') as f:
-    long_description = '\n' + f.read()
+try:
+    long_description = pypandoc.convert('README.md', 'rst')
+    long_description = long_description.replace("\r", "")  # Do not forget this line
+except OSError:
+    print("Pandoc not found. Long_description conversion failure.")
+    import io
+
+    # pandoc is not installed, fallback to using raw contents
+    with io.open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
@@ -37,7 +48,8 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/rix1337/RSScrawler",
     packages=setuptools.find_packages(),
-    data_files=[('web', web),
+    data_files=[('', py),
+                ('web', web),
                 ('web/css', web_css),
                 ('web/img', web_img),
                 ('web/js', web_js)],
