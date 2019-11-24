4de7ac5d5e3c2564710c9a92349f103e05c5de92
rix1337 <>
2019-11-24 20:14:07 +0100

    v.6.1.4 - Automatically create release and upload all assets * includes scripts to create github release and upload assets

--- setup.bat
+++ setup.bat
@@ -5,6 +5,10 @@ python setup.py sdist bdist_wheel
 python -m twine upload dist/* -u __token__ -p %PYPI_TOKEN%
 python rsscrawler/version.py > version.txt
 SET /p version= < version.txt
+setup_create_release.sh github_api_token=%GH_TOKEN% owner=rix1337 repo=rsscrawler version=v.%version%
 pyinstaller --onefile -y -i "rsscrawler/web/img/favicon.ico" --version-file "file_version_info.txt" --add-data "rsscrawler/web;web" "RSScrawler.py" -n "rsscrawler-%version%-standalone-win32"
+setup_upload_asset.sh github_api_token=%GH_TOKEN% owner=rix1337 repo=rsscrawler version=v.%version% filename=./dist/rsscrawler-%version%-standalone-win32.exe
+setup_upload_asset.sh github_api_token=%GH_TOKEN% owner=rix1337 repo=rsscrawler version=v.%version% filename=./dist/rsscrawler-%version%-py3-none-any.whl
 DEL /Q file_version_info.txt
 DEL /Q version.txt
+start "" https://github.com/rix1337/RSScrawler/releases/latest
