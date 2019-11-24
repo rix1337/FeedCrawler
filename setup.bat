16fc4adc5449305cb1971c8f4add60acd955b8d1
rix1337 <>
2019-11-24 20:24:19 +0100

    v.6.1.5 - Automatically trigger Docker build * send POST to docker hub trigger

--- setup.bat
+++ setup.bat
@@ -11,4 +11,5 @@ setup_upload_asset.sh github_api_token=%GH_TOKEN% owner=rix1337 repo=rsscrawler 
 setup_upload_asset.sh github_api_token=%GH_TOKEN% owner=rix1337 repo=rsscrawler version=v.%version% filename=./dist/rsscrawler-%version%-py3-none-any.whl
 DEL /Q file_version_info.txt
 DEL /Q version.txt
-start "" https://github.com/rix1337/RSScrawler/releases/latest
+curl -X POST "https://hub.docker.com/api/build/v1/source/%DH_TRIGGER%/call/"
+start "" https://github.com/rix1337/RSScrawler/releases
