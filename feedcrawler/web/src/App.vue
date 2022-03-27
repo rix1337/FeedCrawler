8d206cd241903194dbb501f5cfbb56830ea7f223
rix1337 <>
2022-03-27 13:44:00 +0200

    v.13.0.0 - Migrate Helper to vue-router

--- feedcrawler/web/src/App.vue
+++ feedcrawler/web/src/App.vue
@@ -7,6 +7,10 @@ if (import.meta.env.MODE === 'development') {
   store.commit("setPrefix", 'http://localhost:9090/feedcrawler/')
 }
 
+// ToDo migrate bootstrap tooltips to vue
+//$(function () {
+//    $('[data-toggle="tooltip"]').tooltip()
+//})
 </script>
 
 <template>
--- feedcrawler/web/src/components/Notifications.vue
+++ feedcrawler/web/src/components/Notifications.vue
@@ -1,3 +1,28 @@
+<script setup>
+// ToDo migrate to vue
+/*function showSuccess(message) {
+    $(".alert-success").html(message).fadeTo(3000, 500).slideUp(500, function () {
+        $(".alert-success").slideUp(500);
+    });
+}
+
+function showInfo(message) {
+    $(".alert-info").html(message).fadeTo(10000, 500).slideUp(500, function () {
+        $(".alert-info").slideUp(500);
+    });
+}
+
+function showInfoLong(message) {
+    $(".alert-info").html(message).show();
+}
+
+function showDanger(message) {
+    $(".alert-danger").html(message).fadeTo(5000, 500).slideUp(500, function () {
+        $(".alert-danger").slideUp(500);
+    });
+}*/
+</script>
+
 <template>
   <div class="alert alert-info container fixed-top" v-text="info" role="alert"
        style="display: none; z-index: 1080"></div>
