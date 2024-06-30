fc5bb1d29220895a445c2d19a867dc3870f0f7b2
rix <rix@rix>
2024-06-30 11:54:51 +0200

    v.20.0.0 - Use vite ESM build instead of CJS

--- feedcrawler/web_interface/vuejs_frontend/vite.config.mjs
+++ feedcrawler/web_interface/vuejs_frontend/vite.config.mjs
@@ -1,7 +1,6 @@
-import {fileURLToPath, URL} from 'url'
-
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'url'
 
 export default defineConfig({
     plugins: [vue()],
