8f5af908270357c34e223b7108cc1f2d62e53f1a
rix <rix@rix>
2025-01-12 19:21:27 +0100

    Improve process exit condition handling
    This should allow for better debugging of #820
    
    Unified "My-JDownloader" naming across codebase for consistency. Enhanced subprocess handling with improved termination logic and added safeguards for graceful shutdown. Addressed minor code errors and improved retry mechanisms for JDownloader connection attempts.

--- feedcrawler/external_tools/myjd_api.py
+++ feedcrawler/external_tools/myjd_api.py
@@ -1,7 +1,7 @@
 # -*- encoding: utf-8 -*-
 # FeedCrawler
 # Projekt von https://github.com/rix1337
-# Dieses Modul stellt die API von My JDownloader in Form eines Python-Moduls zur Verfügung.
+# Dieses Modul stellt die API von My-JDownloader in Form eines Python-Moduls zur Verfügung.
 #
 # Enthält Code von:
 # https://github.com/mmarquezs/My.Jdownloader-API-Python-Library/
